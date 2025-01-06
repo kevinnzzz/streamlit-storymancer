5306c3245579f8436b4963739f171bbe9749eb4f
kevinnzzz <kevindevindioatha@gmail.com>
2025-01-06 20:39:07 +0700

    Update tesapp.py

--- tesapp.py
+++ tesapp.py
@@ -14,7 +14,7 @@ else:
 
 model_alur_path = os.path.join(current_dir, 'modelalurnaivebayes.pkl')
 model_tema_path = os.path.join(current_dir, 'modeltemanaivebayes.pkl')
-vectorizer_path = os.path.join(current_dir, 'model_vectorizer.pkl')
+vectorizer_path = os.path.join(current_dir, 'modelvectorizer.pkl')
 csv_orang_path = os.path.join(current_dir, 'orang.csv')
 csv_tempat_path = os.path.join(current_dir, 'datatempat.csv')
 
